47cc610682fdbee15968894747fd07e8afdc8d2c
Mateusz Kot <mateuszkot@MacBook-Air-Mateusz.local>
2023-09-08 17:40:52 +0200

    Hotfix: Home elements

--- src/components/Additional Technologies/AdditionalTechnologies.js
+++ src/components/Additional Technologies/AdditionalTechnologies.js
@@ -8,12 +8,12 @@ const AdditionalTechnologies = () => {
         <h1 className='additionalTechnologies-heading'>Additional Technologies and Skills</h1>
         <p>Below you can find my additional skills:</p>
         <ul className='additionalTechnologies-list'>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>Responsive Web Design</span></li>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>C1 English</span></li>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>B1 German</span></li>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>Teamwork</span></li>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>Public speaking</span></li>
-            <li><ArrowForwardIcon sx={{color:'#8C61D6'}} fontSize='large'/><span>Canva</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>Responsive Web Design</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>C1 English</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>B1 German</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>Teamwork</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>Public speaking</span></li>
+            <li><ArrowForwardIcon className='additionalTechnologies-icon' sx={{color:'#8C61D6'}}/><span>Canva</span></li>
         </ul>
         </div>
     )
